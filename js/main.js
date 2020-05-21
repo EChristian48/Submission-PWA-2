a68a8383475bb58eb7286f64644c32dc3f0f5f9d
Erick Christian <erickchristian48@gmail.com>
2020-05-21 21:51:25 +0700

    Now dependencies included in 1 corresponding file type

--- js/main.js
+++ js/main.js
@@ -1,5 +1,9 @@
+import '../node_modules/jquery/dist/jquery.js'
+import '../node_modules/@popperjs/core/dist/umd/popper.js'
+import '../node_modules/bootstrap/dist/js/bootstrap.js'
+
 class Program {
-    static function main() {
+    static main() {
 
     }
 }
